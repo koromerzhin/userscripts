a8c81aac96703f7c8755319368441f3098f824ce
Restyled.io <commits@restyled.io>
2021-01-27 09:14:12 +0000

    Restyled by clang-format

--- src/divers/auto_link.js
+++ src/divers/auto_link.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         Auto link _blank packagist
-// @namespace    https://raw.githubusercontent.com/koromerzhin/userscripts/main/src/divers/auto_link.js
+// @namespace
+// https://raw.githubusercontent.com/koromerzhin/userscripts/main/src/divers/auto_link.js
 // @version      0.1
 // @description  try to take over the world!
 // @author       koromerzhin
@@ -9,13 +10,13 @@
 // ==/UserScript==
 
 (function() {
-  'use strict';
-  var links = document.getElementsByTagName("a");
-  var total = links.length;
-  for (var i = 0; i < total; i++) {
-      console.log(links[i].href);
-      if (links[i].href.indexOf("packagist.org") == -1) {
-          links[i].setAttribute('target', '_blank');
-      }
+'use strict';
+var links = document.getElementsByTagName("a");
+var total = links.length;
+for (var i = 0; i < total; i++) {
+  console.log(links[i].href);
+  if (links[i].href.indexOf("packagist.org") == -1) {
+    links[i].setAttribute('target', '_blank');
   }
+}
 })();
